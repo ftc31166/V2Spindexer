cfcacf30e1c1c10e39f9d705b9704ee0c03acbf3
FormulaCarbon <siddhuk.learning@gmail.com>
2024-09-17 21:10:34 -0500

    lift things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Lift.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+// Class for lift Actions
+public class Lift {
+    private DcMotorEx leftPivot;
+    private DcMotorEx rightPivot;
+
+    private DcMotorEx leftExtension;
+    private DcMotorEx rightExtension;
+
+    // TODO: hwmap
+
+    // TODO: Define Actions
+
+    // TODO: Wrap Actions as functions
+
+}
