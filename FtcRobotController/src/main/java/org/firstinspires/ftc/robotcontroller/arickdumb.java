ddb04e1ff7fadf82667454ffb720033aed615323
roobootics <arick@solarflarerobotics.org>
2024-10-06 20:46:23 -0400

    hwaeliufh,zk.cg

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/arickdumb.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/arickdumb.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.robotcontroller;
 
 public class arickdumb {
-    class danidumb{
-        public String explainDynamicKp(){
-            return "so the dynamic kp will ensure that the pid does not throw and um basically basically sdhflsjhfksjahajfhskjhkjsh";
-        }
+    public static String shrek(){
+        return "dah dahahhahhahhqahahahhaahahhaahahahhaahhahaahahahhaahhaahhaahhaahhaahhahahahahahahahahahaahahahahahahahahahyfdhsg,zvkluyvg";
+    }
+}
+class danidumb{
+    public static String explainDynamicKp(){
+        return "so the dynamic kp will ensure that the pid does not throw and um basically basically sdhflsjhfksjahajfhskjhkjsh";
+    }
+    public static void main(String args[]){
+        explainDynamicKp();
+        arickdumb.shrek();
     }
 }
