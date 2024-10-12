199729d8a7fc4fa96d92c8abf8076c6daf3dc843
roobootics <arick@solarflarerobotics.org>
2024-10-12 16:18:23 -0400

    hwaeliufh,zk.cg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TetsyWetsyUwu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TetsyWetsyUwu.java
@@ -14,17 +14,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp
-public class LinkageAndArmAndClawTets extends LinearOpMode{
+public class TetsyWetsyUwu extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
         int target=100;
         int extendo=0;
         int clawpos=1;
+        int btarget=0;
         boolean pressing=false;
         Servo linkage1 = hardwareMap.servo.get("linkage1");
         Servo linkage2 = hardwareMap.servo.get("linkage2");
         Servo claw = hardwareMap.servo.get("claw");
         DcMotorEx arm = hardwareMap.get(DcMotorEx.class,"arm");
+        DcMotorEx buket = hardwareMap.get(DcMotorEx.class,"buket");
         linkage1.setDirection(Servo.Direction.REVERSE);
         linkage1.setPosition(0);
         linkage2.setPosition(0);
@@ -52,15 +54,24 @@ public class LinkageAndArmAndClawTets extends LinearOpMode{
             else{
                 pressing=false;
             }
+            if (gamepad1.left_stick_y>0.2){
+                btarget+=15;
+            }
+            else if (gamepad1.left_stick_y<-0.2){
+                btarget-=15;
+            }
 
             int error1 = (target - arm.getCurrentPosition());
             arm.setPower(-error1 * 0.015);
+            int error2 = (btarget - buket.getCurrentPosition());
+            buket.setPower(-error2 * 0.015);
             linkage1.setPosition(extendo);
             linkage1.setPosition(extendo);
             claw.setPosition(clawpos);
             telemetry.addData("armtarget", target);
             telemetry.addData("extendo", extendo);
             telemetry.addData("clawpos", clawpos);
+            telemetry.addData("bukettarget", btarget);
             telemetry.update();
         }
 
