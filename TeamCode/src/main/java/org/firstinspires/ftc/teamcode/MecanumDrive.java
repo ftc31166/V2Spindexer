5cf26c082d148bf38e399c72ae00b375c9b2a520
PythonAtSea <HudsonGreen27@gmail.com>
2024-10-26 15:22:47 -0500

    Auto works now!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,13 +64,13 @@ public class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 1; // SparkFun OTOS Note: you can probably leave this at 1
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+        public double lateralInPerTick = -0.7954252478940695;
+        public double trackWidthTicks = 9.876567319654294;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
-        public double kA = 0;
+        public double kS = 0.7833764195619626;
+        public double kV = 0.1921529039716232;
+        public double kA = 0.01;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,9 +82,9 @@ public class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 5.0;
+        public double lateralGain = 5.0;
+        public double headingGain = 5.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
@@ -217,18 +217,18 @@ public class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "left_front");
-        leftBack = hardwareMap.get(DcMotorEx.class, "left_back");
-        rightBack = hardwareMap.get(DcMotorEx.class, "right_back");
-        rightFront = hardwareMap.get(DcMotorEx.class, "right_front");
+        leftFront = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        leftBack = hardwareMap.get(DcMotorEx.class, "backLeft");
+        rightBack = hardwareMap.get(DcMotorEx.class, "backRight");
+        rightFront = hardwareMap.get(DcMotorEx.class, "frontRight");
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // TODO: reverse motor directions if needed
-        //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ITDtest.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
+
+@Autonomous
+public final class ITDtest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, beginPose);
+
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .splineToLinearHeading(new Pose2d(10, 30, Math.toRadians(90)), Math.toRadians(0))
+                        .waitSeconds(0.5)
+                        .setTangent(Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(40, 30, Math.toRadians(90)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(55, 50, Math.toRadians(90)), Math.toRadians(-90))
+                        .setReversed(true)
+                        .splineToLinearHeading(new Pose2d(55, 20, Math.toRadians(90)), Math.toRadians(90))
+                        .setReversed(false)
+                        .splineToLinearHeading(new Pose2d(67, 55, Math.toRadians(90)), Math.toRadians(-90))
+                        .setReversed(true)
+                        .splineToLinearHeading(new Pose2d(67, 20, Math.toRadians(90)), Math.toRadians(90))
+                        .setReversed(false)
+                        .splineToLinearHeading(new Pose2d(78, 55, Math.toRadians(90)), Math.toRadians(-90))
+                        .setReversed(true)
+                        .splineToLinearHeading(new Pose2d(78, 15, Math.toRadians(90)), Math.toRadians(90))
+                        .build());
+
+    }
+}
