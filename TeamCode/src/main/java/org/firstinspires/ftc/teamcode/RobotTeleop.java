b6c46319262ad70ecd7f653c13ac1b600eff0651
AdityaVsVik <typius.gamer@gmail.com>
2025-11-08 11:32:48 -0600

    Added Robot Centric Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleop.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class RobotTeleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        Robot robot = new Robot(hardwareMap);
+        Servo hood = hardwareMap.get(Servo.class, "hood");
+
+        ElapsedTime timer = new ElapsedTime();
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            if (gamepad1.a){
+                robot.intake.setPower(Constants.INTAKEINPOWER);
+                robot.feeder.setPower(Constants.FEEDERIN);
+//                robot.reverseFeeder.setPower(robot.feeder.getPower());  un comment this if the ball is leaving the turret before flywheel on
+            }
+            if (gamepad1.b){
+                robot.intake.setPower(0);
+                robot.feeder.setPower(0);
+            }
+            if (gamepad1.dpad_right){
+                hood.setPosition(hood.getPosition()+1);
+            }
+            if (gamepad1.dpad_left){
+                hood.setPosition(hood.getPosition()-1);
+            }
+            if (gamepad1.right_bumper){
+                robot.flywheel.setPower(Constants.SHOOTCLOSE);
+            }
+            if (gamepad1.left_bumper){
+                robot.flywheel.setPower(0);
+            }
+            if (gamepad1.dpad_down){
+                robot.reverseFeeder.setPower(-Constants.FEEDERIN);
+            }
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+}
